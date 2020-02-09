4b21c18dedf20f8fd4273ca6d74c0e12e7b82d77
balabaev00 <balabaev00@yandex.ru>
2020-02-09 18:26:46 +0900

    create

---
+++ Asm-float/Ассемблер №13/main.cpp
@@ -0,0 +1,30 @@
+﻿
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	float l, res;
+	float g = 9.81;
+	float twopi = 6.28;
+	cout << "Введите длину маятника в метрах L=";
+	cin >> l;
+	_asm
+	{
+		fld[g]
+		fld[l]
+		fdiv st(0), st(1)
+		fsqrt
+		fld[twopi]
+		fmul
+		fst[res]
+	}
+	cout << "Результат = " << res << endl;
+	return 0;
+}
+
+
